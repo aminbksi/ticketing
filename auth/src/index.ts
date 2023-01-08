4ba808465774cfb49f6cce8603a6de06bb25edac
Amin Bakhshi <bakhshiamin85@gmail.com>
2023-01-08 19:51:50 +0100

    feat(auth): auth repository init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { json } from "body-parser";
+import { api } from "./api";
 
 const app = express();
 app.use(json());
+
+app.use(api);
 
 app.listen(3000, () => {
   console.log("Listening on 3000");

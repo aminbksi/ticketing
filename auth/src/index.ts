b5a7da262808eec8321899aecd496b2e6d122a97
Amin Bakhshi <bakhshiamin85@gmail.com>
2023-01-09 09:59:15 +0100

    feat(auth): connecting to mongodb in kube cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 import { api } from "./api";
 import { errorHandler } from "./core";
 
@@ -10,6 +11,17 @@ app.use(json());
 app.use(api);
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
+
+start();
